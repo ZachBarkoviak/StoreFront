c277a8f6f2971451796ab31b6dd52b5241cb2f85
ZachBarkoviak <zjbarkoviak@outlook.com>
2022-10-11 15:41:06 -0500

    landing page updated

---
+++ StoreFront.UI.MVC/Models/CartItemViewModel.cs
@@ -0,0 +1,25 @@
+﻿using StoreFront.DATA.EF.Models;
+
+namespace GadgetStore.UI.MVC.Models
+{
+    public class CartItemViewModel
+    {
+        // Shopping cart - Step 2
+        // created this class...
+
+        public int Qty { get; set; }
+
+        public Product CartProd { get; set; }
+
+        public CartItemViewModel() { }
+
+        public CartItemViewModel(int qty, Product product)
+        {
+            Qty = qty;
+            CartProd = product;
+        }
+
+
+
+    }
+}
